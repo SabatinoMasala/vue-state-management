f549e6ce33c276004bcd811c077c3b8dfc8d887f
Sabatino Masala <Masalasabatino@gmail.com>
2025-01-16 14:34:21 +0100

    Basic setup

---
+++ src/components/Navbar.vue
@@ -0,0 +1,51 @@
+<template>
+  <Disclosure as="nav" class="bg-white shadow" v-slot="{ open }">
+    <div class="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
+      <div class="flex h-16 justify-between">
+        <div class="flex">
+          <div class="-ml-2 mr-2 flex items-center md:hidden">
+            <!-- Mobile menu button -->
+            <DisclosureButton class="relative inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-gray-100 hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500">
+              <span class="absolute -inset-0.5" />
+              <span class="sr-only">Open main menu</span>
+              <Bars3Icon v-if="!open" class="block size-6" aria-hidden="true" />
+              <XMarkIcon v-else class="block size-6" aria-hidden="true" />
+            </DisclosureButton>
+          </div>
+          <div class="hidden md:flex md:space-x-8">
+            <a href="#" class="inline-flex items-center border-b-2 border-indigo-500 px-1 pt-1 text-sm font-medium text-gray-900">Home</a>
+          </div>
+        </div>
+        <div class="flex items-center">
+          <div class="hidden md:ml-4 md:flex md:shrink-0 md:items-center">
+            <button type="button" class="relative rounded-full bg-white p-1 text-gray-400 hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2">
+              <span class="absolute -inset-1.5" />
+              <span class="sr-only">View notifications</span>
+              <ShoppingCartIcon class="size-6" aria-hidden="true" />
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <DisclosurePanel class="md:hidden">
+      <div class="space-y-1 pb-3 pt-2">
+        <DisclosureButton as="a" href="#" class="block border-l-4 border-indigo-500 bg-indigo-50 py-2 pl-3 pr-4 text-base font-medium text-indigo-700 sm:pl-5 sm:pr-6">Home</DisclosureButton>
+      </div>
+      <div class="border-t border-gray-200 pb-3 pt-4">
+        <div class="flex items-center px-4 sm:px-6">
+          <button type="button" class="relative ml-auto shrink-0 rounded-full bg-white p-1 text-gray-400 hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2">
+            <span class="absolute -inset-1.5" />
+            <span class="sr-only">View cart</span>
+            <ShoppingCartIcon class="size-6" aria-hidden="true" />
+          </button>
+        </div>
+      </div>
+    </DisclosurePanel>
+  </Disclosure>
+</template>
+
+<script setup>
+import {Disclosure, DisclosureButton, DisclosurePanel} from '@headlessui/vue'
+import {Bars3Icon, ShoppingCartIcon, XMarkIcon} from '@heroicons/vue/24/outline'
+</script>
---
+++ src/components/ProductCard.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="group relative flex flex-col overflow-hidden rounded-lg border border-gray-200 bg-white">
+    <div class="flex flex-1 flex-col space-y-2 p-4">
+      <h3 class="text-sm font-medium text-gray-900">
+        {{ product.title }}
+      </h3>
+      <p class="text-sm text-gray-500">
+        {{ product.description }}
+      </p>
+      <div class="flex flex-1 flex-col justify-end">
+        <p class="text-base font-medium text-gray-900">
+          {{ formattedPrice }}
+        </p>
+      </div>
+      <div class="flex justify-center space-x-2">
+        <Button>
+          <MinusIcon />
+        </Button>
+        <Button>
+          <PlusIcon />
+        </Button>
+      </div>
+    </div>
+  </div>
+</template>
+<script setup>
+import {MinusIcon, PlusIcon} from '@heroicons/vue/24/outline'
+import {Button} from '@/components/ui/button';
+import {computed} from "vue";
+
+const props = defineProps({
+  product: {
+    type: Object,
+    required: true
+  }
+})
+
+const formattedPrice = computed(() => {
+  return `$${props.product.price.toFixed(2)}`;
+})
+
+</script>
