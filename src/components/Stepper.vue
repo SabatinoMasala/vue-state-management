4b389eea2ff8259a1ba7907fa401068fb615b063
Sabatino Masala <Masalasabatino@gmail.com>
2025-01-16 16:46:41 +0100

    Stepper

---
+++ src/components/Stepper.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="flex justify-center items-center space-x-2">
+    <Button @click="$emit('decrement')">
+      <MinusIcon />
+    </Button>
+    <slot></slot>
+    <Button @click="$emit('increment')">
+      <PlusIcon />
+    </Button>
+  </div>
+</template>
+<script setup>
+import {Button} from '@/components/ui/button';
+import {MinusIcon, PlusIcon} from "@heroicons/vue/24/outline";
+</script>
