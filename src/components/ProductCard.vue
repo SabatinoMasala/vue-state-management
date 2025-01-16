48bb5f8f9c86207b94b0a70fe5098627fa073377
Sabatino Masala <Masalasabatino@gmail.com>
2025-01-16 15:06:34 +0100

    Basic MVP

--- src/components/ProductCard.vue
+++ src/components/ProductCard.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="group relative flex flex-col overflow-hidden rounded-lg border border-gray-200 bg-white">
+  <div class="group relative flex flex-col overflow-hidden rounded-lg border border-gray-200" :class="[amount > 0 ? 'bg-amber-100' : 'bg-white']">
     <div class="flex flex-1 flex-col space-y-2 p-4">
       <h3 class="text-sm font-medium text-gray-900">
         {{ product.title }}
@@ -12,11 +12,14 @@
           {{ formattedPrice }}
         </p>
       </div>
-      <div class="flex justify-center space-x-2">
-        <Button>
+      <div class="flex justify-center items-center space-x-2">
+        <Button @click="$emit('decrement', product)">
           <MinusIcon />
         </Button>
-        <Button>
+        <span class="text-sm font-medium text-gray-900">
+          {{ amount }}
+        </span>
+        <Button @click="$emit('increment', product)">
           <PlusIcon />
         </Button>
       </div>
@@ -27,8 +30,13 @@
 import {MinusIcon, PlusIcon} from '@heroicons/vue/24/outline'
 import {Button} from '@/components/ui/button';
 import {computed} from "vue";
+import { format } from '@/lib/number';
 
 const props = defineProps({
+  cart: {
+    type: Array,
+    required: true
+  },
   product: {
     type: Object,
     required: true
@@ -36,7 +44,12 @@ const props = defineProps({
 })
 
 const formattedPrice = computed(() => {
-  return `$${props.product.price.toFixed(2)}`;
+  return format(props.product.price);
+})
+
+const amount = computed(() => {
+  const item = props.cart.find((item) => item.id === props.product.id);
+  return item ? item.quantity : 0;
 })
 
 </script>
