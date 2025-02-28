9b9e6cb3912d3150be6ed7e552588c58c5429031
Sabatino Masala <Masalasabatino@gmail.com>
2025-02-28 16:25:56 +0100

    Initial

--- src/main.js
+++ src/main.js
@@ -2,8 +2,12 @@ import './assets/index.css'
 
 import { createApp } from 'vue'
 import App from './App.vue'
+import {createPinia} from 'pinia';
 import router from './router'
 
+const pinia = createPinia();
+
 createApp(App)
+    .use(pinia)
     .use(router)
     .mount('#app')
