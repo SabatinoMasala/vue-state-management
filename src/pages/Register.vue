de13538bc55d97605ef3d6cccd0aad0bc3349884
Sabatino Masala <Masalasabatino@gmail.com>
2025-03-15 12:55:19 +0100

    Add routes

---
+++ src/pages/Register.vue
@@ -0,0 +1,40 @@
+<template>
+  <form @submit.stop.prevent="handleRegister" class="w-full h-screen flex items-center justify-center px-4">
+    <Card class="w-full max-w-sm">
+      <CardHeader>
+        <CardTitle class="text-2xl">Register</CardTitle>
+        <CardDescription>
+          Enter your details below to register your account.
+        </CardDescription>
+      </CardHeader>
+      <CardContent class="grid gap-4">
+        <div class="grid gap-2">
+          <Label for="name">Name</Label>
+          <Input id="name" type="text" placeholder="Sabatino" required />
+        </div>
+        <div class="grid gap-2">
+          <Label for="email">Email</Label>
+          <Input id="email" type="email" placeholder="m@example.com" required />
+        </div>
+        <div class="grid gap-2">
+          <Label for="password">Password</Label>
+          <Input id="password" type="password" required />
+        </div>
+      </CardContent>
+      <CardFooter>
+        <Button class="w-full">Register</Button>
+      </CardFooter>
+    </Card>
+  </form>
+</template>
+<script setup>
+import {Button} from '@/components/ui/button';
+import {Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle,} from '@/components/ui/card';
+import {Input} from '@/components/ui/input';
+import {Label} from '@/components/ui/label';
+
+const handleRegister = () => {
+  alert('Login');
+}
+
+</script>
--- src/router.js
+++ src/router.js
@@ -1,11 +1,15 @@
-import { createWebHistory, createRouter } from 'vue-router'
+import {createRouter, createWebHistory} from 'vue-router'
 
 import Home from './pages/Home.vue'
 import Checkout from './pages/Checkout.vue'
+import Login from "@/pages/Login.vue";
+import Register from "@/pages/Register.vue";
 
 const routes = [
     { path: '/', component: Home },
     { path: '/checkout', component: Checkout },
+    { path: '/login', component: Login },
+    { path: '/register', component: Register },
 ]
 
 const router = createRouter({
