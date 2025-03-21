21fdc1a84c19b0bdb18bbb76c7c7304e325dd793
Sabatino Masala <Masalasabatino@gmail.com>
2025-03-21 13:26:35 +0100

    Init

---
+++ src/pages/Profile.vue
@@ -0,0 +1,25 @@
+<template>
+  <Navbar />
+  <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-8">
+    <h1 class="text-3xl font-bold">Profile</h1>
+    <p class="mt-2 text-sm text-gray-500">Welcome, {{ userStore.user.name }}!</p>
+    <div class="mt-3">
+      <Button @click="logout">Logout</Button>
+    </div>
+  </div>
+</template>
+<script setup>
+import {Button} from "@/components/ui/button/index.js";
+import Navbar from "@/components/Navbar.vue";
+import {useUserStore} from "@/stores/User.js";
+import {useRouter} from "vue-router";
+
+const router = useRouter();
+
+const userStore = useUserStore();
+
+const logout = async () => {
+  // TODO
+}
+
+</script>
